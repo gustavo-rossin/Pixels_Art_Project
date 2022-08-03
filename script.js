802440048de10f1718a3b24974d509ca2a12dfbf
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-08-03 18:21:08 -0300

    made changes to step 7, made the code shorter and simpler

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-function moveSelected (event) {
-  const box1 = document.querySelector('.box1');
-  const box2 = document.querySelector('.box2');
-  const box3 = document.querySelector('.box3');
-  const box4 = document.querySelector('.box4');
-  box1.classList.remove('selected');
-  box2.classList.remove('selected');
-  box3.classList.remove('selected');
-  box4.classList.remove('selected');
+// Adicionado JS no projeto com a função para remover e adicionar a classe selected ao clicar em cada box. #7
+function moveSelected(event) {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
