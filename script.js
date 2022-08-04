b8a88bcae5fa05d5543a0ec8758ea63150e38e86
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-08-04 11:44:54 -0300

    step 8 completed

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ box1.addEventListener('click', moveSelected);
 box2.addEventListener('click', moveSelected);
 box3.addEventListener('click', moveSelected);
 box4.addEventListener('click', moveSelected);
+
+// Criando função para selecionar a cor e preencher os pixels no quadrado #8
+function pixelColorSelector(event) {
+  const selected = document.querySelector('.selected');
+  const styleFunction = window.getComputedStyle(selected, null);
+  const backgroundColor = styleFunction.getPropertyValue('background-color');
+  event.target.style.backgroundColor = backgroundColor;
+}
+const pixel = document.querySelectorAll('.pixel');
+for (let i = 0; i < pixel.length; i += 1) {
+  pixel[i] = pixel[i].addEventListener('click', pixelColorSelector);
+}
+
