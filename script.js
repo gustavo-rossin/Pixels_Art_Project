cdc4591758a6a531651ab7eddef5bfca5fd7a250
gustavo-rossin <gustavo.rossin@hotmail.com>
2023-02-07 15:48:18 -0300

    commited last changes on project

--- script.js
+++ script.js
@@ -15,6 +15,36 @@ box2.addEventListener('click', moveSelected);
 box3.addEventListener('click', moveSelected);
 box4.addEventListener('click', moveSelected);
 
+
+
+// Criando botão que retorne a cor do quadro para a cor inicial #9
+btnReset = document.getElementById('clear-board');
+function removeColor() {
+  const removePixelColor = document.querySelectorAll('.pixel');
+  for (let i = 0; i < removePixelColor.length; i += 1) {
+    removePixelColor[i].style.backgroundColor = 'white';
+};
+}
+btnReset.addEventListener('click', removeColor);
+
+// criando botão para ter tamanho dinâmico de grid de quadrados
+
+const pixelBoard = document.querySelector('#pixel-board');
+const sizeElement = document.querySelector('#board-size');
+const size = sizeElement.value;
+const generateBoard = document.querySelector('#generate-board');
+
+function gridSize(grid) {
+  pixelBoard.style.setProperty('--size', grid);
+  pixelBoard.innerHTML = ' ';
+  for (let i = 0; i < grid * grid; i += 1) {
+    const div = document.createElement('div');
+    div.classList.add('pixel');
+    pixelBoard.appendChild(div);
+  }
+};
+gridSize(size);
+
 // Criando função para selecionar a cor e preencher os pixels no quadrado #8
 function pixelColorSelector(event) {
   const selected = document.querySelector('.selected');
@@ -26,13 +56,3 @@ const pixel = document.querySelectorAll('.pixel');
 for (let i = 0; i < pixel.length; i += 1) {
   pixel[i] = pixel[i].addEventListener('click', pixelColorSelector);
 }
-
-// Criando botão que retorne a cor do quadro para a cor inicial #9
-btnReset = document.getElementById('clear-board');
-function removeColor() {
-  const removePixelColor = document.querySelectorAll('.pixel');
-  for (let i = 0; i < removePixelColor.length; i += 1) {
-    removePixelColor[i].style.backgroundColor = 'white';
-};
-}
-btnReset.addEventListener('click', removeColor);
